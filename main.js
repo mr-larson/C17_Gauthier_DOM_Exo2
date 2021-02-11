0e2e064ee598211a313f67c07dfc3504559690a4
GAUTHIER <gauthierdewit@gmail.com>
2021-02-11 22:24:36 +0100

    exo2

---
+++ main.js
@@ -0,0 +1,15 @@
+//01
+let myBody = document.getElementsByTagName  ("body") [0]
+//02
+console.log (myBody.firstElementChild)
+//03
+console.log (myBody.lastElementChild)
+//04
+let exo4 = document.getElementsByTagName ("div") [0]
+console.log (exo4.childNodes)
+//05
+let li0 = document.getElementsByTagName ("li") [0]
+console.log (li0.nextElementSibling)
+//06
+let li1 = document.getElementsByTagName ("li") [1]
+console.log (li1.previousElementSibling)
